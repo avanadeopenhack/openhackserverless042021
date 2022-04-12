a0de3dfe5ff1f2ee73117646ec37c75cf0cbf74f
Romain Ehret <romain.ehret@avanade.com>
2022-04-12 15:43:36 +0200

    GetRatings implementation

---
+++ IceCreams.Ratings/Infra/Dto/Rating.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace IceCreams.Ratings.Infra.Dto
+{
+    public class Rating
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("userId")]
+        public string UserId { get; set; }
+
+        [JsonProperty("productId")]
+        public string ProductId { get; set; }
+
+        [JsonProperty("timestamp")]
+        public DateTimeOffset Timestamp { get; set; }
+
+        [JsonProperty("locationName")]
+        public string LocationName { get; set; }
+
+        [JsonProperty("rating")]
+        public int RatingScore { get; set; }
+
+        [JsonProperty("userNotes")]
+        public string UserNotes { get; set; }
+    }
+}
