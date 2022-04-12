02c1269a2528b7503c9a490cfd2b4474ec424a13
Olivier Delmotte <odeatavanade@users.noreply.github.com>
2022-04-12 10:25:10 +0200

    get OK

--- ode/PostHttpTrigger.cs
+++ ode/PostHttpTrigger.cs
@@ -12,14 +12,31 @@ namespace ode.Function
 {
     public static class PostHttpTrigger
     {
-        [FunctionName("GetHttpTrigger")]
-        public static async Task<IActionResult> Run(
+        [FunctionName(nameof(GetHttpTrigger))]
+        public static async Task<IActionResult> GetHttpTrigger(
             [HttpTrigger(AuthorizationLevel.Function, "get", Route = "products/{id:int?}")] HttpRequest req, int? id,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
+            int cleanId = 0;
+            var found = true;
+            if (id != null)
+            {
+                cleanId = id.Value;
+            }
+            else
+            {
+                if (req.Query.ContainsKey("id"))
+                {
+                    found = int.TryParse(req.Query["id"][0], out cleanId);
+                }
+                else
+                {
+                    found = false;
+                }
+            }
 
-            var responseMessage = $"The product name for your product id {id} is Starfruit Explosion";
+            var responseMessage = found ? $"The product name for your product id {cleanId} is Starfruit Explosion" : "No product ID";
 
             return new OkObjectResult(responseMessage);
         }
