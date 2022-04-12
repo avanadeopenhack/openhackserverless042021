c316ad77099245146ff0a5612e0cc9fcb4f1ed81
Ali Lengrai <ali.lengrai@avanade.com>
2022-04-12 14:55:14 +0200

    GetRating(s)

--- IceCreams.Ratings/Functions/GetRating.cs
+++ IceCreams.Ratings/Functions/GetRating.cs
@@ -2,6 +2,7 @@ using System.IO;
 using System.Net;
 using System.Threading.Tasks;
 using IceCreams.Ratings.Managers;
+using IceCreams.Ratings.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
@@ -29,11 +30,11 @@ namespace IceCreams.Ratings.Functions
         [OpenApiParameter(name: "name", In = ParameterLocation.Query, Required = true, Type = typeof(string), Description = "The **Name** parameter")]
         [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/plain", bodyType: typeof(string), Description = "The OK response")]
         public async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req, RatingModel model,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
-
+            
             string name = req.Query["name"];
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
--- IceCreams.Ratings/Functions/GetRatings.cs
+++ IceCreams.Ratings/Functions/GetRatings.cs
@@ -29,7 +29,7 @@ namespace IceCreams.Ratings.Functions
         [OpenApiParameter(name: "name", In = ParameterLocation.Query, Required = true, Type = typeof(string), Description = "The **Name** parameter")]
         [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/plain", bodyType: typeof(string), Description = "The OK response")]
         public async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
