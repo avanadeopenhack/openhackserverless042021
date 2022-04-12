43df85b2ad718a99c4209a8fee868a1d76c75ca1
Ali Lengrai <ali.lengrai@avanade.com>
2022-04-12 15:49:56 +0200

    Fix

--- IceCreams.Ratings/Managers/IRatingManager.cs
+++ IceCreams.Ratings/Managers/IRatingManager.cs
@@ -8,7 +8,5 @@ namespace IceCreams.Ratings.Managers
     public interface IRatingManager
     {
         Task CreateAsync(RatingModel model);
-        Task<IEnumerable<RatingModel>> GetAllAsync();
-        Task<RatingModel> GetAsync(Guid id);
     }
 }
