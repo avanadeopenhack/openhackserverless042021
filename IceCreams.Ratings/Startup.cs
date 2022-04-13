30879fd87c0ba736e0585e4889ad0ab2a8a14399
Olivier Delmotte <odeatavanade@users.noreply.github.com>
2022-04-13 11:18:07 +0200

    configuration key

--- IceCreams.Ratings/Startup.cs
+++ IceCreams.Ratings/Startup.cs
@@ -57,28 +57,21 @@ namespace IceCreams.Ratings
         {
 
             var azureServiceTokenProvider = new AzureServiceTokenProvider();
-
-            // Authenticate to the Azure Resource Manager to get the Service Managed token.
             string accessToken = await azureServiceTokenProvider.GetAccessTokenAsync("https://management.azure.com/");
 
-
             var cosmosDbAccount = config["cosmosDBAccount"];
-            // Setup the List Keys API to get the Azure Cosmos DB keys.
             string endpoint = $"https://management.azure.com/subscriptions/{config["subscriptionId"]}/resourceGroups/{config["resourceGroupName"]}/providers/Microsoft.DocumentDB/databaseAccounts/{cosmosDbAccount}/listKeys?api-version=2019-12-12";
 
             var httpClient = new HttpClient();
-            // Add the access token to request headers.
             httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
 
-            // Post to the endpoint to get the keys result.
             var result = await httpClient.PostAsync(endpoint, new StringContent(""));
 
-            // Get the result back as a DatabaseAccountListKeysResult.
             var keys = await result.Content.ReadFromJsonAsync<DatabaseAccountListKeysResult>();
             
-
             var res = new List<KeyValuePair<string, string>>();
             res.Add(new KeyValuePair<string, string>("CosmosDBConnection", $"AccountEndpoint=https://{cosmosDbAccount}.documents.azure.com:443/;AccountKey={keys.primaryMasterKey};"));
+            res.Add(new KeyValuePair<string, string>("Values:CosmosDBConnection", $"AccountEndpoint=https://{cosmosDbAccount}.documents.azure.com:443/;AccountKey={keys.primaryMasterKey};"));
             return res;
         }
 
